cbc0af7887ead8a3e0c7abc6640fe21ad27fa598
7700Abhishek <7700.abhishekdubey02@gmail.com>
2025-11-11 16:11:33 +0530

    improved

--- first-server-PR-1/index.js
+++ first-server-PR-1/index.js
@@ -3,16 +3,20 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
 
-    if(req.url === "/"){
-        res.end("Welcome Home");
-    } else if(req.url === "/about"){
-        res.end("About Me");
-    } else if(req.url === "/skills"){
-        res.end("My skills");
-    } else if(req.url === "/portfolio"){
-        res.end("My Portfolio");
-    } else if(req.url === "/journey"){
-        res.end("My Journey");
+    if (req.url === "/") {
+        res.end("<h1>Welcome Home</h1>");
+    } else if (req.url === "/about") {
+        const about = "Myself Abhishek and i'm Full Stack Web Developer"
+        res.end(`<h1>${about}</h1>`);
+    } else if (req.url === "/skills") {
+        const skills = "HTML, CSS, JS"
+        res.end(`<h1>My skills are : ${skills}</h1>`);
+    } else if (req.url === "/portfolio") {
+        const port = "and Here are some of my works";
+        res.end(`<h1>My Portfolio ${port}</h1>`);
+    } else if (req.url === "/journey") {
+        const journey = "Here is my coding journey how i learnt things";
+        res.end(`<h1>${journey}</h1>`);
     } else {
         res.end("See You Soon...");
     }
@@ -20,7 +24,7 @@ const server = http.createServer((req, res) => {
     let log = `Date: ${new Date().toLocaleString()} URL : ${req.url}\n`
 
     fs.appendFile("./logs.txt", log, (err) => {
-        if(err){
+        if (err) {
             console.log(err);
         }
     })
